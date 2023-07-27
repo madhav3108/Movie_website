a2e9ad291f70e2eb3a0c4cc5b3df48e009817738
Madhav Singh <88900164+madhav3108@users.noreply.github.com>
2023-07-27 08:22:48 +0530

    Update firebase-config.js

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -4,13 +4,13 @@ import {getAuth} from 'firebase/auth'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDq_4H9-nQQvfp5IumVzPBcq8OcYNmeASo",
-  authDomain: "netflix-6a381.firebaseapp.com",
-  projectId: "netflix-6a381",
-  storageBucket: "netflix-6a381.appspot.com",
-  messagingSenderId: "276600089672",
-  appId: "1:276600089672:web:eae34f4c204436a433bc7d",
-  measurementId: "G-X3Y72MBYVQ"
+ apiKey:ENTER_YOUR_API_KEY, 
+ authDomain:ENTER_YOUR_API_KEY,
+ projectId:ENTER_YOUR_API_KEY,
+ storageBucket:ENTER_YOUR_API_KEY, 
+ messagingSenderId:ENTER_YOUR_API_KEY, 
+ appId:ENTER_YOUR_API_KEY,
+ measurementId:ENTER_YOUR_API_KEY
 };
 
 
